dc4b1d13b70fcb5038db1733615fd52e92508078
kurt landrito <klandrito@academic.rrc.ca>
2026-04-22 02:10:56 -0500

    Reworked the entire frontend directory to use modern version. Used Vite to handle the frontend packaging/ bundling.

--- financial_planner/financial_planner/settings.py
+++ financial_planner/financial_planner/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
     'api',
     'financial_planner',
     'frontend',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +56,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',  # Vite's dev server
 ]
 
 ROOT_URLCONF = 'financial_planner.urls'
