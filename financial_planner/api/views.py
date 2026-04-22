4a76348597d165c1ee2533f89a7f6257fe4fe336
kurt landrito <klandrito@academic.rrc.ca>
2026-04-22 00:36:22 -0500

    Added temporary views for the api The steps in developing api should be: Models -> Serializer -> Views <-> Url -> Frontend

--- financial_planner/api/views.py
+++ financial_planner/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class CreateExpense(APIView):
+    def post(self, request, format=None):
+        pass
+
+class GetExpense(APIView):
+    def get(self, request, format=None):
+        pass
+
+class CreateUser(APIView):
+    def post(self, request, format=None):
+        pass
+
+class LoginUser(APIView):
+    def get(self, request, format=None):
+        pass
+
+class CreateExpenseCategory(APIView):
+    def post(self, request, format=None):
+        pass
+
+class getExpenseCategory(APIView):
+    def get(self, request, format=None):
+        pass
+
+class CreateAsset(APIView):
+    def post(self, request, format=None):
+        pass
+
+class CreateLiability(APIView):
+    def post(self, request, format=None):
+        pass
